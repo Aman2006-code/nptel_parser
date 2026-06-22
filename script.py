77dd5040ee6e35e82bfdfef204d0628d7d42e23b
Aman TS <amants2006@gmail.com>
2026-06-22 19:12:35 +0530

    completed the script

--- script.py
+++ script.py
@@ -36,20 +36,20 @@ data_part = data[0][:-2]
 # Minimum Mark = 17
 # Max != Min => min < average < max
 
-max = int(data_part[-2:])
-min = int(data_part[-4:-2])
-if max == 0:
-    max = 100
-    min = int(data_part[-5:-3])
-if min == max or not (min < average < max):
-    if average != min:
-        min = min % 10
-        if max == 100:
+max_mark = int(data_part[-2:])
+min_mark = int(data_part[-4:-2])
+if max_mark == 0:
+    max_mark = 100
+    min_mark = int(data_part[-5:-3])
+if min_mark == max_mark or not (min_mark < average < max_mark):
+    if average != min_mark:
+        min_mark = min_mark % 10
+        if max_mark == 100:
             data_part = data_part[:-4]
         else:
             data_part = data_part[:-3]
 else:
-    if max == 100:
+    if max_mark == 100:
         data_part = data_part[:-5]
     else:
         data_part = data_part[:-4]
@@ -61,20 +61,30 @@ else:
 
 
 def crack(dataString, cracked_code):
-    if not dataString:
-        if cracked_code:
-            if cracked_code[0] >= cracked_code[1] >= cracked_code[2]:
-                if (
-                    cracked_code[3]
-                    + cracked_code[4]
-                    + cracked_code[5]
-                    + cracked_code[6]
-                    == cracked_code[2]
-                ):
-                    return cracked_code
+    if len(cracked_code) == 9:
+        if not dataString:
+            if cracked_code:
+                if cracked_code[0] >= cracked_code[1] >= cracked_code[2]:
+                    if (
+                        cracked_code[3]
+                        + cracked_code[4]
+                        + cracked_code[5]
+                        + cracked_code[6]
+                        == cracked_code[2]
+                    ):
+                        return cracked_code
+        return None
+
+    for i in range(1, min(len(dataString) + 1, 6)):
+        curr = int(dataString[:i])
+        rem = dataString[i:]
+
+        res = crack(rem, cracked_code + [curr])
+        if res:
+            return res
 
 
-cracked = crack(data_string, [])
+cracked = crack(data_part, [])
 
 header = [
     "Enrolled",
@@ -92,13 +102,13 @@ header = [
     "Standard Deviation",
 ]
 
-res = {}
+result = {}
 for i in range(len(cracked)):
-    res[header[i]] = cracked[i]
+    result[header[i]] = cracked[i]
 
-res["Maximum Mark"] = max
-res["Minimum Mark"] = min
-res["Average Mark"] = average
-res["Standard Deviation"] = std_dev
+result["Maximum Mark"] = max_mark
+result["Minimum Mark"] = min_mark
+result["Average Mark"] = average
+result["Standard Deviation"] = std_dev
 
-print(res)
+print(result)
