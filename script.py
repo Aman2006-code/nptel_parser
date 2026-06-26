c1b8b54f2d2886f5a7c82d5b5275dd86046cec8f
Aman TS <amants2006@gmail.com>
2026-06-26 18:51:52 +0530

    Added features to calculate parameters and plot graph from course statistics data parsed from webpage

--- script.py
+++ script.py
@@ -1,169 +1,37 @@
-import matplotlib.pyplot as plt
 import pandas as pd
 
 import utils
 
 course_key = utils.load_file()
 
-course, course_id, course_abbr = utils.getSubjectIdAndAbbrivation(course_key)
+done = "y"
+result_list = []
 
-course_stats = utils.get_course_stats(course_id)
+while done.lower() != "n":
+    course, course_id, course_abbr = utils.getSubjectIdAndAbbrivation(course_key)
+    done = utils.validator(input("Do you want to add another subject? (Y/n): "))
+    course_stats = utils.get_course_stats(course_id)
+    if course_stats:
+        res = utils.average_stat(course_stats)
 
-if course_stats is not None:
-    print(f"Course stats for {course} ({course_abbr}):")
-    print(course_stats)
-
-# def validator(parameter):
-#     if parameter.lower() == "n" or parameter.lower() == "":
-#         return "n"
-#     elif parameter.lower() == "y":
-#         return "y"
-#     else:
-#         parameter = input("Invalid input. Please enter a valid option (y/N): ")
-#         return validator(parameter)
-
-
-# done = "n"
-
-# header = [
-#     "Enrolled",
-#     "Registered",
-#     "Certified",
-#     "Gold",
-#     "Silver",
-#     "Elite",
-#     "Success",
-#     "Participation",
-#     "Toppers",
-#     "Minimum Mark",
-#     "Maximum Mark",
-#     "Average Mark",
-#     "Standard Deviation",
-# ]
+    else:
+        print("Some Error occured")
+        if done == "n":
+            done = utils.validator(input("Do you want to retry? (Y/n): "))
+        break
+    if res:
+        res = utils.add_parameters(res, course_abbr)
+        result_list.append(res)
+    else:
+        print("Not enough Data...Try re-entering the course...")
+        if done == "n":
+            done = utils.validator(input("Do you want to retry? (Y/n): "))
+        break
 
 
-# result_list = {}
-# while done.lower() != "y":
-#     subject, subject_abbr = get_subject()
-#     data = get_data()
-#     if not data or not subject:
-#         continue
-#     res = {}
-#     for i in range(len(data)):
-#         res[header[i]] = float(data[i])
+if done.lower() == "n":
+    df = pd.DataFrame(result_list)
+    df.rename(columns={"index": "Subject"}, inplace=True)
 
-#     registered = res["Registered"]
-
-#     res["cert_percent"] = (res["Certified"] / registered) * 100
-#     res["gold_percent"] = (res["Gold"] / registered) * 100
-#     res["performance_percent"] = (
-#         (res["Gold"] + res["Silver"] + res["Elite"]) / registered
-#     ) * 100
-#     res["success_percent"] = (res["Success"] / registered) * 100
-
-#     base_score = (
-#         (res["cert_percent"] * 0.85)
-#         + (res["performance_percent"] * 0.70)
-#         + (res["gold_percent"] * 0.50)
-#         + (res["Average Mark"] * 1.00)
-#     )
-
-#     std_dev_penalty = res["Standard Deviation"] * 1.5
-#     enrollment_penalty = min(20, res["Enrolled"] * 0.005)
-
-#     final_score = base_score - std_dev_penalty - enrollment_penalty
-#     res["score"] = max(0, round(final_score, 2))
-#     res["Subject Abbreviation"] = subject_abbr
-
-#     if subject in result_list:
-#         print("Recheck Entry!! Possible duplication")
-#     else:
-#         result_list[subject] = res
-
-#     done = input("Is this the last subject? (y/N): ")
-#     done = validator(done)
-
-# if done.lower() == "y":
-#     df = pd.DataFrame.from_dict(result_list, orient="index").reset_index()
-#     df.rename(columns={"index": "Subject"}, inplace=True)
-
-#     calculated_columns = [
-#         col
-#         for col in df.columns
-#         if col not in ["Subject", "Subject Abbreviation"] + header
-#     ]
-#     columns_order = ["Subject", "Subject Abbreviation"] + header + calculated_columns
-#     df = df[columns_order]
-
-#     df.to_excel("nptel_statistics.xlsx", index=False)
-#     print("Statistics saved to nptel_statistics.xlsx")
-
-#     fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(12, 10))
-
-#     subjects = df["Subject Abbreviation"].tolist()
-
-#     df.plot(
-#         x="Subject",
-#         y=calculated_columns,
-#         kind="line",
-#         marker="o",
-#         ax=axes[0],
-#         colormap="viridis",
-#     )
-#     axes[0].set_title(
-#         "Calculated Metrics & Score by Subject", fontsize=14, fontweight="bold"
-#     )
-#     axes[0].set_ylabel("Value", fontsize=12)
-#     axes[0].legend(loc="upper right")
-
-#     axes[0].tick_params(axis="x", which="both", bottom=False, labelbottom=False)
-#     axes[0].set_xlabel("")
-
-#     for line in axes[0].lines:
-#         for x_val, y_val in zip(line.get_xdata(), line.get_ydata()):
-#             idx = int(x_val)
-#             if 0 <= idx < len(subjects):
-#                 axes[0].annotate(
-#                     subjects[idx],
-#                     xy=(x_val, y_val),
-#                     xytext=(0, 6),
-#                     textcoords="offset points",
-#                     ha="center",
-#                     va="bottom",
-#                     fontsize=9,
-#                     fontweight="semibold",
-#                 )
-
-#     df.plot(
-#         x="Subject",
-#         y="Standard Deviation",
-#         kind="line",
-#         marker="o",
-#         color="tomato",
-#         ax=axes[1],
-#         legend=False,
-#     )
-#     axes[1].set_title("Standard Deviation by Subject", fontsize=14, fontweight="bold")
-#     axes[1].set_ylabel("Standard Deviation", fontsize=12)
-
-#     axes[1].tick_params(axis="x", which="both", bottom=False, labelbottom=False)
-#     axes[1].set_xlabel("")
-
-#     for line in axes[1].lines:
-#         for x_val, y_val in zip(line.get_xdata(), line.get_ydata()):
-#             idx = int(x_val)
-#             if 0 <= idx < len(subjects):
-#                 axes[1].annotate(
-#                     subjects[idx],
-#                     xy=(x_val, y_val),
-#                     xytext=(0, 6),
-#                     textcoords="offset points",
-#                     ha="center",
-#                     va="bottom",
-#                     fontsize=9,
-#                     fontweight="semibold",
-#                 )
-
-#     plt.tight_layout()
-#     plt.savefig("nptel_charts.png", dpi=300)
-#     plt.show()
+    utils.save_to_excel(df)
+    utils.plot(df)
--- utils.py
+++ utils.py
@@ -1,8 +1,38 @@
 import json
 import re
 
+import matplotlib.pyplot as plt
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+
+# For Excel
+header = [
+    "Enrolled",
+    "Registered",
+    "Certified",
+    "Gold",
+    "Silver",
+    "Elite",
+    "Success",
+    "Participation",
+    "Toppers",
+    "max_mark",
+    "min_mark",
+    "average",
+    "standard_deviation",
+]
+
+
+# To create a loop to ask for as many as input as user wishes.
+def validator(parameter):
+    if parameter.lower() == "y" or parameter.lower() == "":
+        return "y"
+    elif parameter.lower() == "n":
+        return "n"
+    else:
+        parameter = input("Invalid input. Please enter a valid option (y/N): ")
+        return validator(parameter)
 
 
 # Don't use this function directly in script.py. Use load_file() instead...
@@ -88,7 +118,7 @@ def getSubjectIdAndAbbrivation(courses):
                     for i in range(len(institutes_offering)):
                         print(f"{i + 1}: {institutes_offering[i]}")
                     institute_code = input(
-                        f"Choose your institute code for your institute of interest from the above institutes offering {subject}"
+                        f"Choose your institute code for your institute of interest from the above institutes offering {subject} : "
                     )
                     if institute_code.isdigit() and int(institute_code) in range(
                         1, len(institutes_offering) + 1
@@ -96,80 +126,13 @@ def getSubjectIdAndAbbrivation(courses):
                         state = True
                     while state == False:
                         institute_code = input(
-                            f"Choose a valid institute offering {subject}"
+                            f"Choose a valid institute offering {subject} : "
                         )
                         if institute_code.isdigit() and int(institute_code) in range(
                             1, len(institutes_offering) + 1
                         ):
                             state = True
-                    course_id = info[int(institute_code) - 1]["id"]
-                    return subject_name, course_id, abbrivate_subject(subject_name)
-            raise ValueError("Subject not found. Please enter a valid subject.")
-        except ValueError as e:
-            print(f"Error: {e}. Please enter a valid subject.")
-        except Exception:
-            print("Please enter a valid subject.")
-
-
-# Don't use this in script.py.
-def abbrivate_subject(subject_name):
-    words = [
-        w
-        for w in subject_name.split()
-        if w.lower() not in ["and", "to", "for", "of", "in"]
-    ]
-    if len(words) >= 2:
-        return "".join([word[0].upper() for word in words])
-    else:
-        return subject_name
-
-
-# This is to get subject of intrest input from the user.
-def getSubjectIdAndAbbrivation(courses):
-    checker = {sub.lower(): (sub, info) for sub, info in courses.items()}
-    while True:
-        try:
-            subject = input("Enter subject: ").strip()
-            if not subject:
-                print("Skipping subject...")
-                return None, None, None
-            if subject.lower() in checker:
-                subject_name, info_list = checker[subject.lower()]
-                if len(info_list) == 1:
-                    course_id = info_list[0]["id"]
-                    return subject_name, course_id, abbrivate_subject(subject_name)
-                else:
-                    institutes_offering = []
-                    for x in info_list:
-                        if x["institute"].startswith("IIT "):
-                            institute_abbr = "IIT " + x["institute"][4]
-                        elif x["institute"].startswith("IITT "):
-                            institute_abbr = "IIIT " + x["institute"][5]
-                        elif x["institute"].startswith("IISc "):
-                            institute_abbr = "IISc Banglore"
-                        else:
-                            institute_abbr = x["institute"]
-                        institutes_offering.append(institute_abbr)
-                    state = False
-                    print(f"Institute Code : Institute Offering {subject}")
-                    for i in range(len(institutes_offering)):
-                        print(f"{i + 1}: {institutes_offering[i]}")
-                    institute_code = input(
-                        f"Choose your institute code for your institute of interest from the above institutes offering {subject}"
-                    )
-                    if institute_code.isdigit() and int(institute_code) in range(
-                        1, len(institutes_offering) + 1
-                    ):
-                        state = True
-                    while state == False:
-                        institute_code = input(
-                            f"Choose a valid institute offering {subject}"
-                        )
-                        if institute_code.isdigit() and int(institute_code) in range(
-                            1, len(institutes_offering) + 1
-                        ):
-                            state = True
-                    course_id = info[int(institute_code) - 1]["id"]
+                    course_id = info_list[int(institute_code) - 1]["id"]
                     return subject_name, course_id, abbrivate_subject(subject_name)
             raise ValueError("Subject not found. Please enter a valid subject.")
         except ValueError as e:
@@ -191,3 +154,134 @@ def get_course_stats(course_id):
     except Exception as e:
         print(f"Error: {e}")
         return None
+
+
+# For calculating the mean in case of data from multiple years available
+def average_stat(stat):
+    # Cleaning
+    course_stats = stat["data"][0]["run_wise_stats"]
+    # Mean calculation
+    df = pd.DataFrame(course_stats)
+    df = df.drop(columns=["Timeline", "noc_courseid"]).astype(float)
+    df = df.mean().to_dict()
+    if df:
+        return df
+    else:
+        print("Corrupt data")
+        return None
+
+
+# To add helper parameters to the subject's stat list (calculated from existing data)
+def add_parameters(result, abbr):
+    res = result.copy()
+    registered = res["Registered"]
+
+    res["cert_percent"] = (res["Certified"] / registered) * 100
+    res["gold_percent"] = (res["Gold"] / registered) * 100
+    res["performance_percent"] = (
+        (res["Gold"] + res["Silver"] + res["Elite"]) / registered
+    ) * 100
+    res["success_percent"] = (res["Success"] / registered) * 100
+
+    base_score = (
+        (res["cert_percent"] * 0.85)
+        + (res["performance_percent"] * 0.70)
+        + (res["gold_percent"] * 0.50)
+        + (res["average"] * 1.00)
+    )
+
+    std_dev_penalty = res["standard_deviation"] * 1.5
+    enrollment_penalty = min(20, res["Enrolled"] * 0.005)
+    final_score = base_score - std_dev_penalty - enrollment_penalty
+    res["score"] = max(0, round(final_score, 2))
+    res["Subject Abbreviation"] = abbr
+
+    return res
+
+
+# To save the list of selected subject as excel sheet
+def save_to_excel(df):
+    calculated_columns = [
+        col
+        for col in df.columns
+        if col not in ["Subject", "Subject Abbreviation"] + header
+    ]
+    columns_order = ["Subject", "Subject Abbreviation"] + header + calculated_columns
+    df = df[columns_order]
+
+    df.to_excel("nptel_statistics.xlsx", index=False)
+    print("Statistics saved to nptel_statistics.xlsx")
+
+
+# To plot the data and save as png for chosen subject
+
+
+def plot(df):
+    fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(12, 10))
+    subjects = df["Subject Abbreviation"].tolist()
+
+    df.plot(
+        x="Subject",
+        y=calculated_columns,
+        kind="line",
+        marker="o",
+        ax=axes[0],
+        colormap="viridis",
+    )
+    axes[0].set_title(
+        "Calculated Metrics & Score by Subject", fontsize=14, fontweight="bold"
+    )
+    axes[0].set_ylabel("Value", fontsize=12)
+    axes[0].legend(loc="upper right")
+
+    axes[0].tick_params(axis="x", which="both", bottom=False, labelbottom=False)
+    axes[0].set_xlabel("")
+
+    for line in axes[0].lines:
+        for x_val, y_val in zip(line.get_xdata(), line.get_ydata()):
+            idx = int(x_val)
+            if 0 <= idx < len(subjects):
+                axes[0].annotate(
+                    subjects[idx],
+                    xy=(x_val, y_val),
+                    xytext=(0, 6),
+                    textcoords="offset points",
+                    ha="center",
+                    va="bottom",
+                    fontsize=9,
+                    fontweight="semibold",
+                )
+
+    df.plot(
+        x="Subject",
+        y="Standard Deviation",
+        kind="line",
+        marker="o",
+        color="tomato",
+        ax=axes[1],
+        legend=False,
+    )
+    axes[1].set_title("Standard Deviation by Subject", fontsize=14, fontweight="bold")
+    axes[1].set_ylabel("Standard Deviation", fontsize=12)
+
+    axes[1].tick_params(axis="x", which="both", bottom=False, labelbottom=False)
+    axes[1].set_xlabel("")
+
+    for line in axes[1].lines:
+        for x_val, y_val in zip(line.get_xdata(), line.get_ydata()):
+            idx = int(x_val)
+            if 0 <= idx < len(subjects):
+                axes[1].annotate(
+                    subjects[idx],
+                    xy=(x_val, y_val),
+                    xytext=(0, 6),
+                    textcoords="offset points",
+                    ha="center",
+                    va="bottom",
+                    fontsize=9,
+                    fontweight="semibold",
+                )
+
+    plt.tight_layout()
+    plt.savefig("nptel_charts.png", dpi=300)
+    plt.show()
